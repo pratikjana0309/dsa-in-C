aebd5adaccc9c173370ce1be8887ccd8d305a03c
pratikjana0309 <157054364+pratikjana0309@users.noreply.github.com>
2024-03-10 23:35:47 +0530

    Create leetcode.c

---
+++ LAB4/leetcode.c
@@ -0,0 +1,25 @@
+/**
+*	Definition for singly-linked list.
+*	struct ListNode {
+*	int val;
+*	struct ListNode *next;
+* };
+*/
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) { if(headA==NULL||headB==NULL){
+return NULL;
+}
+struct ListNode* a=headA; struct ListNode* b=headB; while(a!=b){
+if(a==NULL){
+a=headB;
+}
+else{
+a=a->next;
+} if(b==NULL){
+b=headA;
+}
+else{
+b=b->next;
+}
+}
+return a;
+}
